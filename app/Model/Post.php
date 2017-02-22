ecce2a05fb3a244316ec9cc078441dd0e7cc6728
天久秀樹 <amekuhideki@amahisahideki-no-Mac-mini.local>
2017-02-22 12:49:30 +0900

    add relation definition

--- app/Model/Post.php
+++ app/Model/Post.php
@@ -64,4 +64,22 @@ class Post extends AppModel {
 		)
 	);
 
+	public $hasAndBelongsToMany = array(
+		'Tag' => array(
+			'className' => 'tag',
+			'joinTable' => 'posts_tags',
+			'foreignKey' => 'post_id',
+			'associationForeignKey' => 'tag_id',
+			'unique' => true,
+			'conditions' => '',
+			'fields' => '',
+			'order' => '',
+			'limit' => '',
+			'offset' => '',
+			'finderQuery' => '',
+			'deleteQuery' => '',
+			'insertQuery' => '',
+ 		)
+	);
+
 }
