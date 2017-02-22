64647f7dd5122906c371bf8570512942457cd530
天久秀樹 <amekuhideki@amahisahideki-no-Mac-mini.local>
2017-02-22 12:48:09 +0900

    add tags and posts_tags table

---
+++ app/Test/Fixture/PostsTagFixture.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * PostsTag Fixture
+ */
+class PostsTagFixture extends CakeTestFixture {
+
+/**
+ * Fields
+ *
+ * @var array
+ */
+	public $fields = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false, 'key' => 'primary'),
+		'post_id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false),
+		'tag_id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false),
+		'created' => array('type' => 'datetime', 'null' => true, 'default' => null),
+		'modified' => array('type' => 'datetime', 'null' => true, 'default' => null),
+		'indexes' => array(
+			'PRIMARY' => array('column' => 'id', 'unique' => 1)
+		),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
+	);
+
+/**
+ * Records
+ *
+ * @var array
+ */
+	public $records = array(
+		array(
+			'id' => 1,
+			'post_id' => 1,
+			'tag_id' => 1,
+			'created' => '2017-02-22 12:09:26',
+			'modified' => '2017-02-22 12:09:26'
+		),
+	);
+
+}
---
+++ app/Test/Fixture/TagFixture.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Tag Fixture
+ */
+class TagFixture extends CakeTestFixture {
+
+/**
+ * Fields
+ *
+ * @var array
+ */
+	public $fields = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false, 'key' => 'primary'),
+		'tag' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'tagSlug' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'created' => array('type' => 'datetime', 'null' => true, 'default' => null),
+		'modified' => array('type' => 'datetime', 'null' => true, 'default' => null),
+		'indexes' => array(
+			'PRIMARY' => array('column' => 'id', 'unique' => 1)
+		),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
+	);
+
+/**
+ * Records
+ *
+ * @var array
+ */
+	public $records = array(
+		array(
+			'id' => 1,
+			'tag' => 'Lorem ipsum dolor sit amet',
+			'tagSlug' => 'Lorem ipsum dolor sit amet',
+			'created' => '2017-02-22 12:09:35',
+			'modified' => '2017-02-22 12:09:35'
+		),
+	);
+
+}
