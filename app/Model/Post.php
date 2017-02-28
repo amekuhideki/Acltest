b0b912b3fb089a14b06dc88db4acef8a73b2b03d
天久秀樹 <amekuhideki@amahisahideki-no-Mac-mini.local>
2017-02-28 16:38:08 +0900

    Fix

--- app/Model/Post.php
+++ app/Model/Post.php
@@ -6,35 +6,6 @@ App::uses('AppModel', 'Model');
  * @property User $User
  */
 class Post extends AppModel {
-	// public function createWithAttachments($data) {
-	// 		// Sanitize your images before adding them
-	// 		$images = array();
-	// 		if (!empty($data['Image'][0])) {
-	// 				foreach ($data['Image'] as $i => $image) {
-	// 						if (is_array($data['Image'][$i])) {
-	// 								// Force setting the `model` field to this model
-	// 								$image['model'] = 'Post';
-	//
-	// 								// Unset the foreign_key if the user tries to specify it
-	// 								if (isset($image['foreign_key'])) {
-	// 										unset($image['foreign_key']);
-	// 								}
-	//
-	// 								$images[] = $image;
-	// 						}
-	// 				}
-	// 		}
-	// 		$data['Image'] = $images;
-	//
-	// 		// Try to save the data using Model::saveAll()
-	// 		$this->create();
-	// 		if ($this->saveAll($data)) {
-	// 				return true;
-	// 		}
-	//
-	// 		// Throw an exception for the controller
-	// 		throw new Exception(__("This post could not be saved. Please try again"));
-	// }
 
 /**
  * Validation rules
