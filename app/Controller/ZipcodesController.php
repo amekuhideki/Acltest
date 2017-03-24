098d6f9e7fd8a0505d226cd4c9c7c02cd7f4f742
天久秀樹 <amekuhideki@amahisahideki-no-Mac-mini.local>
2017-03-24 12:14:39 +0900

    Add function insert user infomation into database of another model

--- app/Controller/ZipcodesController.php
+++ app/Controller/ZipcodesController.php
@@ -41,4 +41,17 @@ class ZipcodesController extends AppController {
 		}
 	}
 
+	public function add(){
+		if ($this->request->is('post')){
+			$Member = ClassRegistry::init('Members');
+			$Member->create();
+			if ($Member->saveAll($this->request->data)) {
+				$this->Flash->success(__('The post has been saved.'));
+				return $this->redirect(array('action' => 'index'));
+			} else {
+				$this->Flash->error(__('The post could not be saved. Please, try again.'));
+			}
+		}
+	}
+
 }
