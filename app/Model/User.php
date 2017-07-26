3570ce012984c018f27f55109d93896716e12f56
天久秀樹 <h_ameku@funteam.co.jp>
2017-07-26 16:19:54 +0900

    Fix validation

--- app/Model/User.php
+++ app/Model/User.php
@@ -27,28 +27,28 @@ class User extends AppModel {
     return true;
   }
 
-	public $belongsTo = array('Group');
-	public $actsAs = array('Acl' => array('type' => 'requester', 'enabled' => false));
+  public $belongsTo = array('Group');
+  public $actsAs = array('Acl' => array('type' => 'requester', 'enabled' => false));
 
-	public function parentNode() {
-		if (!$this->id && empty($this->data)) {
-				return null;
-		}
-		if (isset($this->data['User']['group_id'])) {
-				$groupId = $this->data['User']['group_id'];
-		} else {
-				$groupId = $this->field('group_id');
-		}
-		if (!$groupId) {
-				return null;
-		} else {
-				return array('Group' => array('id' => $groupId));
-		}
-	}
+  public function parentNode() {
+    if (!$this->id && empty($this->data)) {
+        return null;
+    }
+    if (isset($this->data['User']['group_id'])) {
+        $groupId = $this->data['User']['group_id'];
+    } else {
+        $groupId = $this->field('group_id');
+    }
+    if (!$groupId) {
+        return null;
+    } else {
+        return array('Group' => array('id' => $groupId));
+    }
+  }
 
-	public function bindNode($user) {
+  public function bindNode($user) {
     return array('model' => 'Group', 'foreign_key' => $user['User']['group_id']);
-	}
+  }
   
   public function passwordConfirm($check) {
     if ($this->data['User']['password'] === $this->data['User']['password_confirm']) {
@@ -62,10 +62,10 @@ class User extends AppModel {
  *
  * @var array
  */
-	public $validate = array(
-		'username' => array(
-			'notBlank' => array(
-				'rule' => array('notBlank'),
+  public $validate = array(
+    'username' => array(
+      'notBlank' => array(
+        'rule' => array('notBlank'),
       ),
       'check_character' => array(
         'rule' => array('check_char'),
@@ -75,19 +75,36 @@ class User extends AppModel {
     'email' => array(
       'notBlank' => array(
         'rule' => array('notBlank'),
-			),
+      ),
       'unique' => array(
         'rule' => 'isUnique',
         'required' => true,
         'allowEmpty' => false,
         'message' => '既に使用されています'
       ),
-		),
-		'password' => array(
-			'notBlank' => array(
-				'rule' => array('notBlank'),
+      'mail_address' => array(
+        'rule' => array('email'),
+        'message' => '正しいアドレスを入力してください'
+      ),
+      // 'is_email' => array(
+      //   'rule' => array('mail_check'),
+      //   'message' => '正しいアドレスを入力してください'
+      // )
+    ),
+    'phone_number' => array(
+      'nunber_check' => array(
+        'required' => false,
+        'allowEmpty' => true,
+        'rule' => array('number_check'),
+        'message' => '正しい値で入力してください'
+      ),
+      
+    ),
+    'password' => array(
+      'notBlank' => array(
+        'rule' => array('notBlank'),
         'message' => 'パスワードを入力してください'
-			),
+      ),
       'match' => array(
         'rule' => 'passwordConfirm',
         'message' => 'パスワードが一致していません'
@@ -96,7 +113,7 @@ class User extends AppModel {
         'rule' => array('minLength', 4),
         'message' => 'パスワードは4文字以上にしてください'
       )
-		),
+    ),
     'password_confirm' => array(
       'match' => array(
         'rule' => array(
@@ -105,11 +122,11 @@ class User extends AppModel {
         )
       )
     ),
-		'group_id' => array(
-			'numeric' => array(
-				'rule' => array('numeric'),
-			),
-		),
+    'group_id' => array(
+      'numeric' => array(
+        'rule' => array('numeric'),
+      ),
+    ),
     'credentials_token' => array(
       'notBlank' => array(
         'rule' => array('notBlank'),
@@ -120,40 +137,50 @@ class User extends AppModel {
         'rule' => array('notBlank'),
       )
     ),
-	);
+  );
   public function check_char($check) {
     $character = array_shift($check);
-    $first_char = mb_substr($character, 0, 1);
-    if (preg_match("/^[ぁ-ゞ]+$/u", $first_char)) {
-      return preg_match("/^[ぁ-ゞ]+$/u", $character);
+    return preg_match("/^\p{Hiragana}+$|^\p{Katakana}+$|^[a-zA-Z]+$|\p{Han}/u", $character);
+  }
+  
+  public function number_check($check) {
+    $value = array_shift($check);
+    // $number = mb_convert_kana($value,"a", "utf-8");
+    if (ctype_digit($value) && (strlen($value) == 10 || strlen($value) == 11)) {
+      return true;
+    } else {
+      return false;
     }
-    if (preg_match("/^[ァ-ヾ]+$/u", $first_char)) {
-      return preg_match("/^[ァ-ヾ]+$/u", $character);
+  }
+  
+  public function mail_check($check) {
+    $email = array_shift($check);
+    $mailre = '^[\x01-\x7F]+@(([-a-z0-9]+\.)*[a-z]+|\[\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\])';
+    if (preg_match($mailre, $email)) {
+      return preg_match($mailre, $email);
     }
-    if (preg_match("/^[a-zA-Z]+$/", $first_char)) {
-      return preg_match("/^[a-zA-Z]+$/", $character);
-    }
+    exit;
   }
 /**
  * hasMany associations
  *
  * @var array
  */
-	public $hasMany = array(
-		'Post' => array(
-			'className' => 'Post',
-			'foreignKey' => 'user_id',
-			'dependent' => false,
-			'conditions' => '',
-			'fields' => '',
-			'order' => '',
-			'limit' => '',
-			'offset' => '',
-			'exclusive' => '',
-			'finderQuery' => '',
-			'counterQuery' => ''
-		)
-	);
+  public $hasMany = array(
+    'Post' => array(
+      'className' => 'Post',
+      'foreignKey' => 'user_id',
+      'dependent' => false,
+      'conditions' => '',
+      'fields' => '',
+      'order' => '',
+      'limit' => '',
+      'offset' => '',
+      'exclusive' => '',
+      'finderQuery' => '',
+      'counterQuery' => ''
+    )
+  );
   
   public $hasOne = array(
     'userImage' => array(
